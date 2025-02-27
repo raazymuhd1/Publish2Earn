a5d511d13dfde83b49a3cc3605deb88f48fb6e40
raazymuhd1 <muhammadrazi51@gmail.com>
2025-02-28 00:26:29 +0700

    added an input on write post page

--- src/app/(post)/write/page.tsx
+++ src/app/(post)/write/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+import {useState} from 'react'
+import { Editor } from 'primereact/editor';
 
 const WritePage = () => {
+    const [text, setText] = useState("")
+
   return (
     <section className="h-full w-[90%] mx-auto">
-       <h2> Write a post </h2>
+       <Editor 
+          className="bg-textMain h-screen rounded-[10px] text-[#fff]"
+          value={text} 
+          onTextChange={(e) => {
+            console.log(e.htmlValue!)
+            setText(e?.htmlValue!)
+          }} 
+          style={{ height: '320px' }} />
     </section>
   )
 }
